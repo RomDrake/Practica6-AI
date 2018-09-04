9a4802c5d216517b6054777130ea88822596de1f
Universitario <rommeloscuro@gmail.com>
2018-09-04 19:18:02 -0400

    Subi practica

---
+++ AI_TSV/Assets/Scritps/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    Transform _destination;
+    UnityEngine.AI.NavMeshAgent _navMeshAgent;
+	void Start () {
+        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("La wea no sirve" + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+	}
+	private void SetDestination()
+    {
+        Vector3 targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
